f9b4507929a1bb6fe53a9f2b80a97cbea7f1220c
nurgle <nurgle>
2003-05-26 09:32:15 +0000

    - Rewrote command line parsing with GNU getopt.

--- arnold/src/unix/main.c
+++ arnold/src/unix/main.c
@@ -33,6 +33,7 @@
 #include "display.h"
 #include <string.h>
 #include <unistd.h>
+#include <getopt.h>
 
 /* FIXME: Move declarations to header file */
 extern void	Host_InitDriveLEDIndicator();
@@ -81,11 +82,93 @@ int main(int argc, char *argv[])
 	return 0;	/* Never reached */
 }
 
+	void help_exit() {
+		printf("Switches supported:\n");
+		printf("-drivea <string> = specify disk image to insert into drive A\n");
+		printf("-driveb <string> = specify disk image to insert into drive B\n");
+		printf("-cart <string> = specify CPC+ cartridge to insert\n");				
+		printf("-frameskip <integer> = specify frame skip (0-5)\n");
+		printf("-crtctype <integer> = specify crtc type (0,1,2,3,4)\n");
+		printf("-tape <string> = specify tape image\n");
+		printf("-cpctype <integer> = specify CPC type (0=CPC464, 1=CPC664, 2=CPC6128, 3=CPC464+, 4=CPC6128+\n");
+		printf("-snapshort <string> = specify snapshot to load\n");
+		printf("-kbdtype <integer> = specify keyboard type (0=QWERTY, 1=QWERTZ, 2=AZERTY)");
+		exit(0);
+	}
+
 void init_main(int argc, char *argv[]) {
 	char LocalDirectory[1024];
 	//char ProgramDirectory[1024]="";
 	//char *dataDirectory;
 	char *romDirectory;
+	int kbd = -1;
+	/* name, has_arg, flag, val */
+	static struct option long_options[] = {
+		{"tape", 1, 0, 't'},
+		{"drivea", 1, 0, 'a'},
+		{"driveb", 1, 0, 'b'},
+		{"cart", 1, 0, 'c'},
+		{"frameskip", 1, 0, 'f'},
+		{"crtctype", 1, 0, 'r'},
+		{"cpctype", 1, 0, 'p'},
+		{"snapshot", 1, 0, 's'},
+		{"kbdtype", 1, 0, 'k'},
+		{"help", 0, 0, 'h'},
+		{0, 0, 0, 0}
+	};
+	int c;
+	int digit_optind = 0;
+	char *tape = NULL;
+	char *drivea = NULL;
+	char *driveb = NULL;
+	char *cart = NULL;
+	char *frameskip = NULL;
+	char *crtctype = NULL;
+	char *cpctype = NULL;
+	char *snapshot = NULL;
+	char *kbdtype = NULL;
+	do {
+		int this_option_optind = optind ? optind : 1;
+		int option_index = 0;
+		c = getopt_long_only (argc, argv, "",
+			long_options, &option_index);
+		printf("c: %i %c\n", c, c);
+		switch(c) {
+			case 'h':
+			case '?':
+				help_exit();
+				break;
+			case 't':
+				tape = optarg;
+				break;
+			case 'a':
+				drivea = optarg;
+				break;
+			case 'b':
+				driveb = optarg;
+				break;
+			case 'c':
+				cart = optarg;
+				break;
+			case 'f':
+				frameskip = optarg;
+				break;
+			case 'r':
+				crtctype = optarg;
+				break;
+			case 'p':
+				cpctype = optarg;
+				break;
+			case 's':
+				snapshot = optarg;
+				break;
+			case 'k':
+				kbdtype = optarg;
+				break;
+
+		}
+	} while (c != -1);
+	printf("tape: %s\n", tape);
 
 	romDirectory = getRomDirectory();
 	if (romDirectory == NULL) {
@@ -128,157 +211,83 @@ void init_main(int argc, char *argv[]) {
 		chdir(getDiskDirectory());	/* FIXME: What is this? */
 
 		CPC_SetCPCType(CPC_TYPE_CPC6128);
-
-		if (argc!=0)
-		{
-
-			int argindex = 0;
-
-			do
-			{
-				if (strcmp("-tape",argv[argindex])==0)
-				{
-					if ((argindex+1)<argc)
-					{
-						if
-(!TapeImage_Insert(argv[argindex+1]))
-{
-	printf("Failed to open tape image %s.\n",argv[argindex+1]);
-}
-					}
-				}
-
-				if (strcmp("-drivea",argv[argindex])==0)
-				{
-					if ((argindex+1)<argc)
-					{
-
-if (!GenericInterface_InsertDiskImage(0, argv[argindex+1]))
-{
-	printf("Failed to open disk image %s.\n",argv[argindex+1]);
-} 
-					}
-				}
-
-				if (strcmp("-driveb",argv[argindex])==0)	
-				{
-					if ((argindex+1)<argc)
-					{
-
-if (!GenericInterface_InsertDiskImage(1, argv[argindex+1]))
-{	
-	printf("Failed to open disk image %s.\n",argv[argindex+1]);
-}
-					}
-				}
-
-				if (strcmp("-cart",argv[argindex])==0)
-				{
-					if ((argindex+1)<argc)
-					{
-
-if (!GenericInterface_InsertCartridge(argv[argindex+1]))
-{
-	printf("Failed to insert cartridge %s\n", argv[argindex+1]);
-}
-					}
-				}
-				
-				if (strcmp("-frameskip",argv[argindex])==0)
-				{
-					int frameskip;
-
-					frameskip =  atoi(argv[argindex+1]);
-
-					CPC_SetFrameSkip(frameskip);
-				}
-
-				if (strcmp("-crtctype",argv[argindex])==0)
-				{
-					int crtc;
-				
-					crtc = atoi(argv[argindex+1]);
-				
-					CRTC_SetType(crtc);
-				}
-			
-				if (strcmp("-cpctype", argv[argindex])==0)
-				{
-					int cpctype;
-
-					cpctype = atoi(argv[argindex+1]);
-
-					switch (cpctype)
-					{
-						case 0:
-						{	
-				CPC_SetCPCType(CPC_TYPE_CPC464);
-				}
-				break;
-				case 1:
-				{
-				CPC_SetCPCType(CPC_TYPE_CPC664);
-				}
-				break;
-				case 2:
-				{
-				CPC_SetCPCType(CPC_TYPE_CPC6128);
-				}
-				break;
-				case 3:
-				{
-				CPC_SetCPCType(CPC_TYPE_464PLUS);
-				}
-				break;
-				case 4:
-				{
-				CPC_SetCPCType(CPC_TYPE_6128PLUS);
-				}
-				break;
-
-				case 5:
-				{
-				CPC_SetCPCType(CPC_TYPE_KCCOMPACT);
-				}
-				break;
-
-				default:
-				{
-				CPC_SetCPCType(CPC_TYPE_CPC6128);
-				}
-				break;
-
-				}
-				}
-
-				if (strcmp("-snapshot",argv[argindex])==0)
-				{
-if (!Snapshot_Load(argv[argindex+1]))
-{	
-	printf("Failed to open snapshot %s.\n",argv[argindex+1]);
-}
-
-	
-
-				}
-
-				if (strcmp("-help",argv[argindex])==0)
-				{
-					printf("Switches supported:\n");
-					printf("-drivea <string> = specify disk image to insert into drive A\n");
-					printf("-driveb <string> = specify disk image to insert into drive B\n");
-					printf("-cart <string> = specify CPC+ cartridge to insert\n");				
-					printf("-frameskip <integer> = specify frame skip (0-5)\n");
-					printf("-crtctype <integer> = specify crtc type (0,1,2,3,4)\n");
-					printf("-tape <string> = specify tape image\n");
-					printf("-cpctype <integer> = specify CPC type (0=CPC464, 1=CPC664, 2=CPC6128, 3=CPC464+, 4=CPC6128+\n");
-					printf("-snapshort <string> = specify snapshot to load\n");
-
-				}
-				argindex++;
+		
+		if (tape) {
+			if (!TapeImage_Insert(tape)) {
+				printf("Failed to open tape image %s.\n", tape);
 			}
-			while (argindex<argc);
 		}
+
+		if (drivea) {
+			if (!GenericInterface_InsertDiskImage(0, drivea)) {
+				printf("Failed to open disk image %s.\n",
+					drivea);
+			}
+		}
+
+		if (driveb) {
+			if (!GenericInterface_InsertDiskImage(1, driveb)) {
+				printf("Failed to open disk image %s.\n",
+					driveb);
+			}
+		}
+
+		if (cart) {
+			if (!GenericInterface_InsertCartridge(cart)) {
+				printf("Failed to insert cartridge %s\n", cart);
+			}
+		}
+
+		if (frameskip) {
+			int fskip;
+			fskip =  atoi(frameskip);
+			CPC_SetFrameSkip(fskip);
+		}
+
+		if (crtctype) {
+			int crtc;
+			crtc = atoi(crtctype);
+			CRTC_SetType(crtc);
+		}
+
+		if (cpctype) {
+			int cpc;
+			cpc = atoi(cpctype);
+			switch (cpc)
+			{
+				case 0:
+					CPC_SetCPCType(CPC_TYPE_CPC464);
+					break;
+				case 1:
+					CPC_SetCPCType(CPC_TYPE_CPC664);
+					break;
+				case 2:
+					CPC_SetCPCType(CPC_TYPE_CPC6128);
+					break;
+				case 3:
+					CPC_SetCPCType(CPC_TYPE_464PLUS);
+					break;
+				case 4:
+					CPC_SetCPCType(CPC_TYPE_6128PLUS);
+					break;
+				case 5:
+					CPC_SetCPCType(CPC_TYPE_KCCOMPACT);
+					break;
+				default:
+					CPC_SetCPCType(CPC_TYPE_CPC6128);
+					break;
+			}
+		}
+
+		if (snapshot) {
+			if (!Snapshot_Load(snapshot)) {
+				printf("Failed to open snapshot %s.\n",
+					snapshot);
+			}
+		}
+
+		if ( kbdtype ) kbd = atoi(kbdtype);
+		printf("kbdtype: %i\n",kbd);
 
 #ifdef HAVE_GTK
 		fprintf(stderr, "Initializing GTK+\n");
@@ -298,6 +307,10 @@ if (!Snapshot_Load(argv[argindex+1]))
 		Host_InitDriveLEDIndicator();
 
 		Render_SetDisplayWindowed();
+
+#ifdef HAVE_SDL
+		if (kbd != -1) sdl_InitialiseKeyboardMapping(kbd);
+#endif
 
 		printf("Initialised CPC Emulation Core...\n");
 
